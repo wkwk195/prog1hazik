3342d5082c91c521990e1e75b3ecb1fa5a6e6181
wkwk195 <146947385+wkwk195@users.noreply.github.com>
2026-03-20 12:09:22 +0100

    Add otodikhet_evszam.c to calculate character differences

---
+++ otodikhet_evszam.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+    int ketto = (char)'d' - (char)'b';     
+    int nulla = (char)'a' - (char)'a';     
+    int hat = (char)'g' - (char)'a';       
+    
+   
+    printf("%d%d%d%d\n", ketto, nulla, ketto, hat);
+
+    return nulla;
+}
